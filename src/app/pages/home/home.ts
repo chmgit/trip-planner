01504617e3a3c9c95febf77c2f83306657896460
chana melki <chanamelki26@gmail.com>
2026-06-10 17:30:55 +0300

    Add home page layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Auth, User } from '../../services/auth';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+  currentUser: User | null = null;
+
+  constructor(
+    private auth: Auth,
+    private router: Router
+  ) {
+    this.currentUser = this.auth.getCurrentUser();
+
+    if (!this.currentUser) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
