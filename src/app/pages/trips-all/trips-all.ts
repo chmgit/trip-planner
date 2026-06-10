e5085bc9849d9f40b899420f8bd6b6a07826fccd
chana melki <chanamelki26@gmail.com>
2026-06-10 17:37:11 +0300

    Add all trips page

--- src/app/pages/trips-all/trips-all.ts
+++ src/app/pages/trips-all/trips-all.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Trips, Trip } from '../../services/trips';
 
 @Component({
   selector: 'app-trips-all',
-  imports: [],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './trips-all.html',
-  styleUrl: './trips-all.css',
+  styleUrl: './trips-all.css'
 })
-export class TripsAll {}
+export class TripsAll {
+  trips: Trip[] = [];
+  errorMessage = '';
+
+  constructor(private tripsService: Trips) {}
+
+  ngOnInit(): void {
+    this.tripsService.getTrips().subscribe({
+      next: (trips) => {
+        this.trips = trips;
+      },
+      error: () => {
+        this.errorMessage = 'שגיאה בטעינת הטיולים';
+      }
+    });
+  }
+}
--- src/app/services/trips.ts
+++ src/app/services/trips.ts
@@ -1,4 +1,30 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Service()
-export class Trips {}
+export interface Trip {
+  id: string;
+  name: string;
+  destination: string;
+  date: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Trips {
+  private apiUrl = 'http://localhost:3000/trips';
+
+  constructor(private http: HttpClient) {}
+
+  getTrips(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.apiUrl);
+  }
+
+  getTripById(id: string): Observable<Trip> {
+    return this.http.get<Trip>(`${this.apiUrl}/${id}`);
+  }
+}
