ad39a31183a0ba217e781166c7f96cbbd3379de3
chana melki <chanamelki26@gmail.com>
2026-06-10 18:32:31 +0300

    Add all trips with images

--- src/app/pages/trip-details/trip-details.ts
+++ src/app/pages/trip-details/trip-details.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Trips, Trip } from '../../services/trips';
 
 @Component({
   selector: 'app-trip-details',
-  imports: [],
+  imports: [NgIf, RouterLink],
   templateUrl: './trip-details.html',
-  styleUrl: './trip-details.css',
+  styleUrl: './trip-details.css'
 })
-export class TripDetails {}
+export class TripDetails {
+  trip: Trip | null = null;
+  errorMessage = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private tripsService: Trips
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.tripsService.getTripById(id).subscribe({
+        next: (trip) => {
+          this.trip = trip;
+        },
+        error: () => {
+          this.errorMessage = 'שגיאה בטעינת פרטי הטיול';
+        }
+      });
+    }
+  }
+}
--- src/app/pages/trips-all/trips-all.ts
+++ src/app/pages/trips-all/trips-all.ts
@@ -1,26 +1,31 @@
-import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { NgFor, NgIf, JsonPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Trips, Trip } from '../../services/trips';
 
 @Component({
   selector: 'app-trips-all',
-  imports: [NgFor, NgIf, RouterLink],
+  imports: [NgFor, NgIf, RouterLink, JsonPipe],
   templateUrl: './trips-all.html',
   styleUrl: './trips-all.css'
 })
 export class TripsAll {
-  trips: Trip[] = [];
+  trips = signal<Trip[]>([]);
   errorMessage = '';
 
-  constructor(private tripsService: Trips) {}
+  constructor(private tripsService: Trips) { }
 
   ngOnInit(): void {
     this.tripsService.getTrips().subscribe({
-      next: (trips) => {
-        this.trips = trips;
+      next: (tripsFromServer) => {
+        console.log('trips from server:', tripsFromServer);
+
+        this.trips.set(tripsFromServer);
+
+        console.log('trips inside signal:', this.trips());
       },
-      error: () => {
+      error: (error) => {
+        console.error('error loading trips:', error);
         this.errorMessage = 'שגיאה בטעינת הטיולים';
       }
     });
